034702512214656a7ff0004fe8764d7de4fcd309
carloscenasouza-bit <carlos.cena.souza@escola.pr.gov.br>
2026-06-18 09:12:59 -0300

    a

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const botao = document.getElementById('meuBotao');
-
-    // Adiciona um evento de clique ao botão
-    botao.addEventListener('click', () => {
-        alert('O botão funcionou perfeitamente!');
-    });
-});
